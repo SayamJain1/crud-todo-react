6a68738c02fbc4d90adfef088b6a7bd2bdabe001
sanyam jain <sayamjain99300@gmail.com>
2022-11-07 08:45:12 +0530

    code fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import TodoList from "./components/TodoList";
 function App() {
 
   const initialState = JSON.parse(localStorage.getItem('todos')) || [];
-  const [input, setInput] = useState("");
   const [todos, setTodos] = useState(initialState);
   const [editTodo, setEditTodo] = useState(null);
   
@@ -19,8 +18,6 @@ function App() {
       <div className="app-wrapper">
         <div>
           <Form
-            input={input}
-            setInput={setInput}
             todos={todos}
             setTodos={setTodos}
             editTodo={editTodo}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,34 @@
-import {useEffect} from "react";
-import {v4 as uuidv4} from 'uuid'
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+const Form = ({ todos, setTodos, editTodo, setEditTodo }) => {
+  const [input, setInput] = useState("");
 
-    const updateTodo = (title, id, completed) => {
-        const newTodo = todos.map((todo) => 
-            todo.id === id ? {title, id, completed} : todo
-        )
-        setTodos(newTodo)
-        setEditTodo('')
+  const updateTodo = (title, id, completed) => {
+    const newTodo = todos.map((todo) =>
+      todo.id === id ? { title, id, completed } : todo
+    );
+    setTodos(newTodo);
+    setEditTodo("");
+  };
+
+  useEffect(() => {
+    if (editTodo) {
+      setInput(editTodo.title);
+    } else {
+      setInput("");
     }
+  }, [setInput, editTodo]);
 
-    useEffect(() => {
-        if (editTodo) {
-            setInput(editTodo.title);
-        } else {
-            setInput('')
-        }
-    }, [setInput, editTodo])
-
-    const onFormSubmit = (e) => {
-        e.preventDefault()
-        if (!editTodo) {
-                setTodos([...todos, {id : uuidv4(), title: input, completed: false}])
-                setInput('')
-        } else {
-            updateTodo(input, editTodo.id, editTodo.completed)
-        }
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    if (!editTodo) {
+      setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+      setInput("");
+    } else {
+      updateTodo(input, editTodo.id, editTodo.completed);
     }
+  };
   return (
     <form onSubmit={onFormSubmit}>
       <input
@@ -35,10 +36,11 @@ const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
         placeholder="Write Task for Today"
         className="task-input"
         value={input}
+        required
         onChange={(e) => setInput(e.target.value)}
       />
       <button className="button-add" type="submit">
-        {editTodo ? 'OK' : 'Add'}
+        {editTodo ? "OK" : "Add"}
       </button>
     </form>
   );
